f709b734b806ed93a5627445ea6795586a0b10f4
manientezami <m.entezami@bham.ac.uk>
2020-01-24 12:00:37 +0000

    my new script

---
+++ script_me.R
@@ -0,0 +1,8 @@
+cats <- data.frame(coat = c("calico", "black", "tabby"), 
+                   weight = c(2.1, 5.0,3.2), 
+                   likes_string = c(1, 0, 1))
+write.csv(x = cats, file = "data/feline-data.csv", row.names = FALSE)
+
+###################################
+cats <- read.csv(file = "data/feline-data.csv")
+cats
